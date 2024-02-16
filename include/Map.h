ff53af41eea03a4453a3fa55c6b13306045255b4
MicheleMichetti <michele.michetti91@gmail.com>
2024-02-17 00:56:32 +0100

    Created Map.h

---
+++ include/Map.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using tileStdMap = std::map<std::pair,tile::Tile>;
+using entityStdMap = std::map<uint32_t,entity::Entity>;
+
+class Map {
+    private:
+        uint16_t id_;
+        uint8_t currentType_;
+        std::string mapName_;
+        std::string bitmapName_;
+        tileStdMap tiles_; //<Coordinates,tile>
+        entityStdMap entities_;// Map<entityId, Entity> 
+
+    public:
+
+        Map();
+        Map(uint16_t id, uint8_t currentType, std::string mapName, std::string bitmapName, tileStdMap tiles, entityStdMap entities);
+
+        bool doesItCollide(uint16_t desiredX, uint16_t desiredY, uint8_t direction);
+        bool doesItInteract(uint16_t desiredX, uint16_t desiredY, uint8_t direction);
+
+}
