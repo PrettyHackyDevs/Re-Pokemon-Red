1023daa3a9c099642d91b5b66b5ec208d0a489ad
Marc Gavilán <marcgavilangil@gmail.com>
2024-02-17 01:08:37 +0100

    refactor: Coordinate not needed for tile

--- include/Tile.hpp
+++ include/Tile.hpp
@@ -4,14 +4,13 @@
 
 class Tile {
     public:
-        Tile(utils::Coordinate coordinate, uint8_t collision_directions_bitmask, uint8_t interaction_directions_bitmask);
+        Tile(uint8_t collision_directions_bitmask, uint8_t interaction_directions_bitmask);
         ~Tile();
         bool isCollision(utils::Direction player_direction);
         bool isInteraction(utils::Direction player_direction);
         utils::Coordinate getCoordinate();
 
     private:
-        utils::Coordinate coordinate_;
         uint8_t collision_directions_bitmask_;
         uint8_t interaction_directions_bitmask_;
 };
