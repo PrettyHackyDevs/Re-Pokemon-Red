8d2e6f186aed88ce9b182dff497e661f70cd47cb
MicheleMichetti <michele.michetti91@gmail.com>
2024-02-17 02:08:05 +0100

    populated destructor, doesItCollide, doesItInteract

--- src/Map.cpp
+++ src/Map.cpp
@@ -7,11 +7,23 @@ Map::Map(uint16_t id, uint8_t currentType, std::string mapName, std::string bitm
     bitmapName_ = bitmapName;
     tiles_ = tiles;
     entities_ = entities;
+
+}
+
+Map::~Map() {
+    tiles_.clear();
+    entities_.clear();
+    
 }
 
 bool Map::doesItCollide(uint16_t desiredX, uint16_t desiredY, uint8_t direction) {
-    
+    tile::Tile tile = tiles_.at( std::pair<uint16_t,uint16_t> (desiredX, desiredY) );
+    return tile.isCollision(direction);
+
 }
+
 bool Map::doesItInteract(uint16_t desiredX, uint16_t desiredY, uint8_t direction) {
+    tile::Tile tile = tiles_.at( std::pair<uint16_t,uint16_t> (desiredX, desiredY) );
+    return tile.isInteraction(direction);
 
 }
