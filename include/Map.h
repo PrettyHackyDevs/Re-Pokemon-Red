9386af1656885a392920e48da06d44d04215f209
MicheleMichetti <michele.michetti91@gmail.com>
2024-02-18 00:53:39 +0100

    switched tiles_ from std::map to boost::multi_array.

--- include/Map.h
+++ include/Map.h
@@ -6,7 +6,7 @@
 #include <string>
 #include <vector>
 
-using tileStdMap = std::map<std::pair,tile::Tile>;
+using tileArray = boost::multi_array<Tile,2>;
 using entityStdMap = std::map<uint32_t,entity::Entity>;
 
 class Map {
@@ -15,16 +15,35 @@ class Map {
         uint8_t currentType_;
         std::string mapName_;
         std::string bitmapName_;
-        tileStdMap tiles_; //<Coordinates,tile>
+        tileArray tiles_; //<Coordinates,tile>
         entityStdMap entities_;// Map<entityId, Entity> 
 
     public:
 
         Map();
+        Map(const uint16_t &id, const uint8_t &currentType, const std::string &mapName, const std::string &bitmapName, const tileArray &tiles, const uint16_t &dimensionX, const uint16_t &dimensionY, const entityStdMap &entities);
         ~Map();
-        Map(uint16_t id, uint8_t currentType, std::string mapName, std::string bitmapName, tileStdMap tiles, entityStdMap entities);
 
-        bool doesItCollide(uint16_t desiredX, uint16_t desiredY, uint8_t direction);
-        bool doesItInteract(uint16_t desiredX, uint16_t desiredY, uint8_t direction);
+        void setId(const uint16_t &id);
+        void setCurrentType(const uint8_t &currentType);
+        void setMapName(const std::string &mapName);
+        void setBitmapName(const std::string &bitmapName);
+        void setTiles(const tileArray tiles, const uint16_t &dimensionX, const uint16_t &dimensionY);
+        void setEntities(const entityStdMap &entities);
+        void setTile(const utils::Coordinate &coordinate, const Tile &tile);
+
+        const uint16_t getId();
+        const uint8_t getCurrentType();
+        const std::string getMapName();
+        const std::string getBitmapName();
+        const tileArray getTiles();
+        const entityStdMap getEntities();
+
+        void swapTiles(Tile tile1, Tile Tile2);
+
+        bool doesItCollide(const tils::Coordinate &coordinate, const uint8_t &direction);
+        bool doesItInteract(const utils::Coordinate &coordinate, const uint8_t &direction);
+        bool isPokemonBattleTriggered();
+        bool isTrainerBattleTriggered();
 
 }
--- src/Map.cpp
+++ src/Map.cpp
@@ -1,29 +1,79 @@
 #include "Map.h"
 
-Map::Map(uint16_t id, uint8_t currentType, std::string mapName, std::string bitmapName, tileStdMap tiles, entityStdMap entities) {
-    id_ = id;
-    currentType_ = currentType;
-    mapName_ = mapName;
-    bitmapName_ = bitmapName;
-    tiles_ = tiles;
-    entities_ = entities;
+Map::Map(const uint16_t &id, const uint8_t &currentType, const std::string &mapName, const std::string &bitmapName, const tileArray &tiles, const uint16_t &dimensionX, const uint16_t &dimensionY, const entityStdMap &entities) {
+    setId(id);
+    setCurrentType(currentType);
+    setMapName(mapName);
+    setBitmapName(bitmapName);
+    setTiles(tiles, dimensionX, dimensionY);
+    setEntities(entities);
 
 }
 
 Map::~Map() {
-    tiles_.clear();
     entities_.clear();
     
 }
 
-bool Map::doesItCollide(uint16_t desiredX, uint16_t desiredY, uint8_t direction) {
-    tile::Tile tile = tiles_.at( std::pair<uint16_t,uint16_t> (desiredX, desiredY) );
+void Map::setId(const uint16_t &id) {
+    this->id_ = id;
+}
+void Map::setCurrentType(const uint8_t &currentType) {
+    this->currentType_ currentType;
+}
+void Map::setMapName(const std::string &mapName) {
+    this->mapName_ = mapName;
+}
+void Map::setBitmapName(const std::string &bitmapName) {
+    this->bitmapName_ = bitmapName;
+}
+void Map::setTiles(const tileArray &tiles, const uint16_t &dimensionX, const uint16_t &dimensionY) {
+    if(dimensionX == 0 || dimensionY == 0) {
+        return;
+    }
+    tiles_.resize(boost::extents[dimensionX][dimensionY]); 
+    this->tiles_ = tiles;
+}
+void Map::setEntities(const entityStdMap &entities) {
+    this->entities_ = entities;
+}
+void Map::setTile(const utils::Coordinate &coordinate, const Tile &tile) {
+    tiles_[coordinate.x][coordinate.y] = tile;
+}
+
+const uint16_t Map::getId() {
+    return id_;
+}
+const uint8_t Map::getCurrentType() {
+    return currentType_;
+}
+const std::string Map::getMapName() {
+    return mapName_;
+}
+const std::string Map::getBitmapName() {
+    return bitmapName_;
+}
+const tileStdMap Map::getTiles() {
+    return tiles_;
+}
+const entityStdMap Map::getEntities() {
+    return entities_;
+}
+
+void Map::swapTiles(Tile &tile1, Tile &tile2) {
+    tileArray tiletmp = tile1;
+    tile1 = tile2;
+    tile2 = tiletmp;
+}
+
+bool Map::doesItCollide(const utils::Coordinate &coordinate, const uint8_t &direction) {
+    Tile tile = tiles_[coordinate.x][coordinate.y];
     return tile.isCollision(direction);
 
 }
 
-bool Map::doesItInteract(uint16_t desiredX, uint16_t desiredY, uint8_t direction) {
-    tile::Tile tile = tiles_.at( std::pair<uint16_t,uint16_t> (desiredX, desiredY) );
+bool Map::doesItInteract(const utils::Coordinate &coordinate, const uint8_t &direction) {
+    Tile tile = tiles_[coordinate.x][coordinate.y];
     return tile.isInteraction(direction);
 
 }
