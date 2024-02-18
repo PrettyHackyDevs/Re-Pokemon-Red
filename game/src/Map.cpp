bb09edce76b82a3d60169f0524959b19d868fabf
MicheleMichetti <michele.michetti91@gmail.com>
2024-02-18 14:03:15 +0100

    Use consistent variable format "my_variable"

--- game/src/Map.cpp
+++ game/src/Map.cpp
@@ -1,21 +1,21 @@
 #include <Map.hpp>
 
-Map::Map(const uint16_t& id, const uint8_t& currentType, const std::string& mapName, const std::string& bitmapName, const tileArray& tiles, const uint16_t& dimensionX, const uint16_t& dimensionY,
+Map::Map(const uint16_t& id, const uint8_t& current_type, const std::string& map_name, const std::string& bitmap_name, const tileArray& tiles, const uint16_t& dimensionX, const uint16_t& dimensionY,
          const entityMap& entities) {
     setId(id);
-    setCurrentType(currentType);
-    setMapName(mapName);
-    setBitmapName(bitmapName);
+    setCurrentType(current_type);
+    setMapName(map_name);
+    setBitmapName(bitmap_name);
     setTiles(tiles, dimensionX, dimensionY);
     setEntities(entities);
 }
 
-Map::~Map() { entities_.clear(); }
+Map::~Map() {}
 
 void Map::setId(const uint16_t& id) { this->id_ = id; }
-void Map::setCurrentType(const uint8_t& currentType) { this->currentType_ = currentType; }
-void Map::setMapName(const std::string& mapName) { this->mapName_ = mapName; }
-void Map::setBitmapName(const std::string& bitmapName) { this->bitmapName_ = bitmapName; }
+void Map::setcurrentType(const uint8_t& current_type) { this->current_type_ = current_type; }
+void Map::setMapName(const std::string& map_name) { this->map_name_ = map_name; }
+void Map::setBitmapName(const std::string& bitmap_name) { this->bitmap_name_ = bitmap_name; }
 void Map::setTiles(const tileArray& tiles, const uint16_t& dimensionX, const uint16_t& dimensionY) {
     if (dimensionX == 0 || dimensionY == 0) {
         return;
@@ -27,9 +27,9 @@ void Map::setEntities(const entityMap& entities) { this->entities_ = entities; }
 void Map::setTile(const utils::Coordinate& coordinate, const Tile& tile) { tiles_[coordinate.x][coordinate.y] = tile; }
 
 const uint16_t Map::getId() { return id_; }
-const uint8_t Map::getCurrentType() { return currentType_; }
-const std::string Map::getMapName() { return mapName_; }
-const std::string Map::getBitmapName() { return bitmapName_; }
+const uint8_t Map::getCurrentType() { return current_type_; }
+const std::string Map::getMapName() { return map_name_; }
+const std::string Map::getBitmapName() { return bitmap_name_; }
 const tileArray Map::getTiles() { return tiles_; }
 const entityMap Map::getEntities() { return entities_; }
 
