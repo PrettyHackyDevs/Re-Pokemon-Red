cb36e345c7152c96faa8bde5eb31e60e1efa01fb
MicheleMichetti <michele.michetti91@gmail.com>
2024-02-25 17:22:36 +0100

    Created and populated Movement class. Build successful

---
+++ game/include/Movement.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <utils.hpp>
+
+namespace _movement {
+
+class Movement {
+
+protected:
+    utils::Coordinate coordinate_;
+    uint8_t direction_;
+
+public:
+    void setCoordinate(utils::Coordinate coordinate);
+    void setDirection(uint8_t direction);
+    void setX(int16_t x);
+    void setY(int16_t y);
+
+    utils::Coordinate getCoordinate();
+    uint8_t getDirection();
+    int16_t getX();
+    int16_t getY();
+
+    void moveUp();
+    void moveDown();
+    void moveRight();
+    void moveLeft();
+
+
+};
+
+};
---
+++ game/src/Movement.cpp
@@ -0,0 +1,43 @@
+#include<Movement.hpp>
+
+using namespace _movement;
+
+void Movement::setCoordinate(utils::Coordinate coordinate) {
+    coordinate_.x = coordinate.x;
+    coordinate_.y = coordinate.y;
+}
+void Movement::setDirection(uint8_t direction) {
+    direction_ = direction;
+}
+void Movement::setX(int16_t X) {
+    coordinate_.x = X;
+}
+void Movement::setY(int16_t Y) {
+    coordinate_.y = Y;
+}
+
+utils::Coordinate Movement::getCoordinate() {
+    return coordinate_;
+}
+uint8_t Movement::getDirection() {
+    return direction_;
+}
+int16_t Movement::getX() {
+    return coordinate_.x;
+}
+int16_t Movement::getY() {
+    return coordinate_.y;
+}
+
+void Movement::moveUp() {
+    coordinate_.y++;
+}
+void Movement::moveDown() {
+    coordinate_.y--;
+}
+void Movement::moveRight() {
+    coordinate_.x++;
+}
+void Movement::moveLeft() {
+    coordinate_.x--;
+}
