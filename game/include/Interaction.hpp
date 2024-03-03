9b7f2a2d876a2a9e63a41ee93afc5b4874c7c6fa
michelemichetti <michele.michetti91@gmail.com>
2024-03-03 18:50:44 +0000

    Autoformat code

--- game/include/Interaction.hpp
+++ game/include/Interaction.hpp
@@ -3,37 +3,29 @@
 namespace _interaction {
 
 class Interaction {
+   protected:
+    uint32_t id_;
+    uint8_t type_;
+    uint8_t status_bit_mask;
 
-    protected:
-        uint32_t id_;
-        uint8_t type_;
-        uint8_t status_bit_mask;
+   public:
+    Interaction();
+    Interaction(uint32_t id, uint8_t type);
+    ~Interaction();
 
-    public:
+    void setId(uint32_t id_);
+    void setType(uint8_t type_);
+    void setStatus(uint8_t status_bit_mask);
 
-        Interaction();
-        Interaction(uint32_t id, uint8_t type);
-        ~Interaction();
+    uint32_t getId();
+    uint8_t getType();
+    uint8_t getStatus();
 
-        void setId(uint32_t id_);
-        void setType(uint8_t type_);
-        void setStatus(uint8_t status_bit_mask);
-
-        uint32_t getId();
-        uint8_t getType();
-        uint8_t getStatus();
-
-        bool isInteraction();
-        void playInteraction();
-        bool isInteractionEnded();
-        void enableInteraction();
-        void disableInteraction();
-
-
-
+    bool isInteraction();
+    void playInteraction();
+    bool isInteractionEnded();
+    void enableInteraction();
+    void disableInteraction();
 }
 
-
-
-
-}
+}  // namespace _interaction
--- game/include/utils.hpp
+++ game/include/utils.hpp
@@ -8,21 +8,21 @@ struct Coordinate {
     int y;
 };
 
-    enum Direction : uint8_t {
-        up      = 0b00000001,
-        down    = 0b00000010,
-        left    = 0b00000100,
-        right   = 0b00001000,
-    };
+enum Direction : uint8_t {
+    up = 0b00000001,
+    down = 0b00000010,
+    left = 0b00000100,
+    right = 0b00001000,
+};
 
-    enum InteractionType : uint8_t {
-        text_object     = 0b00000001,
-        text_human      = 0b00000010,
-        simple_choice   = 0b00000100,
-        multiple_choice = 0b00001000,
-        collect_object  = 0b00010000,
-        movable_objects = 0b00100000,
-        trigger         = 0b01000000,
-        trainer         = 0b10000000
-    };
-}
+enum InteractionType : uint8_t {
+    text_object = 0b00000001,
+    text_human = 0b00000010,
+    simple_choice = 0b00000100,
+    multiple_choice = 0b00001000,
+    collect_object = 0b00010000,
+    movable_objects = 0b00100000,
+    trigger = 0b01000000,
+    trainer = 0b10000000
+};
+}  // namespace utils
