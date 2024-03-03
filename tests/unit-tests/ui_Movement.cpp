a8ad6cfc964c1ac7e7748da2312adbeb285bc2d2
michelemichetti <michele.michetti91@gmail.com>
2024-03-03 17:16:25 +0100

    First draft ui_movement test

--- tests/unit-tests/ui_Movement.cpp
+++ tests/unit-tests/ui_Movement.cpp
@@ -1,6 +1,8 @@
 #include<Movement.hpp>
 #include "gtest/gtest.h"
 
+using namespace _movement;
+
 utils::Coordinate coordinate;
 uint8_t direction;
 
@@ -10,14 +12,40 @@ void resetState() {
     direction = utils::Direction::none;
 }
 
-int movingUp(int numberOfSteps) {
+bool movingUp(int numberOfSteps) {
     
     resetState();
 
-    //_movement::Movement myMovement(coordinate, direction);
+    Movement my_movement(coordinate, direction);
+    my_movement.moveUp();
+    utils::Coordinate updated_coordinates = my_movement.getCoordinate();
+    uint8_t updated_direction = my_movement.getDirection();
+    utils::Coordinate updated_future_coordinates = my_movement.getFutureCoordinate();
+    uint8_t updated_future_direction = my_movement.getFutureDirection();
 
-    
+    bool result_x = false;
+    if(updated_coordinates.x == updated_future_coordinates.x) {
+        result_x == true;
+    }
+    bool result_y = false;
+    if( (updated_coordinates.y+1) == updated_future_coordinates.y) {
+        result_y == true;
+    }
+    bool result_direction = false;
+    if(updated_direction == utils::Direction::none && updated_future_direction == utils::Direction::up) {
+        result_direction == true;
+    }
+
+    return result_x & result_y & result_direction;
 
     resetState();
+
+}
+
+namespace {
+
+TEST() {
+    EXPECT_EQ(true, movingUp(1));
+}
 
 }
