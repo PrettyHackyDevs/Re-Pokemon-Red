9af686a12a5d41db834deb1d77c763a51ae962ed
Michele Michetti <michele.michetti91@gmail.com>
2024-03-10 15:58:12 +0000

    Autoformat code

--- tests/unit-tests/ui_Movement.cpp
+++ tests/unit-tests/ui_Movement.cpp
@@ -23,31 +23,30 @@ bool MovingUp(int numberOfSteps) {
     utils::Coordinate updated_future_coordinates = my_movement.getFutureCoordinate();
     uint8_t updated_future_direction = my_movement.getFutureDirection();
 
-    std::cout<<"updated_coordinates = "<<updated_coordinates.x<<","<<updated_coordinates.y<<std::endl;
-    std::cout<<"updated_future_coordinates = "<<updated_future_coordinates.x<<","<<updated_future_coordinates.y<<std::endl;
-    std::cout<<"updated_direction = "<<updated_direction<<std::endl;
-    std::cout<<"updated_future_direction = "<<updated_future_direction<<std::endl;
+    std::cout << "updated_coordinates = " << updated_coordinates.x << "," << updated_coordinates.y << std::endl;
+    std::cout << "updated_future_coordinates = " << updated_future_coordinates.x << "," << updated_future_coordinates.y << std::endl;
+    std::cout << "updated_direction = " << updated_direction << std::endl;
+    std::cout << "updated_future_direction = " << updated_future_direction << std::endl;
 
     bool result_x = false;
     if (updated_coordinates.x == updated_future_coordinates.x) {
         result_x == true;
     }
-    std::cout<<result_x<<std::endl;
+    std::cout << result_x << std::endl;
     bool result_y = false;
     if ((updated_coordinates.y + 1) == updated_future_coordinates.y) {
         result_y == true;
     }
-    std::cout<<result_y<<std::endl;
+    std::cout << result_y << std::endl;
     bool result_direction = false;
     if (!(updated_future_direction ^ utils::Direction::up) & 0b11111111) {
         result_direction == true;
     }
-    std::cout<<result_direction<<std::endl;
+    std::cout << result_direction << std::endl;
 
     resetState();
 
     return result_x & result_y & result_direction;
-
 }
 
 namespace {
